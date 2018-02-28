6d8bd38116f53f0f8b071de473b2ed0e3e6363bd
Ahmedjjj <33035022+Ahmedjjj@users.noreply.github.com>
2018-02-28 17:26:07 +0100

    gameboy

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -1,16 +1,29 @@
 package ch.epfl.gameboj;
 
+import ch.epfl.gameboj.component.memory.Ram;
+import ch.epfl.gameboj.component.memory.RamController;
+
 public class GameBoy {
-	
-	
-	public GameBoy(Object cartridge){
-		
-	}
-	
-	
-	public Bus bus() {
-	return null;
-		
-	}
+    private Ram ram;
+    private RamController ramController;
+    private RamController echoRamController;
+    private Bus bus;
+
+    public GameBoy(Object cartridge) {
+        ram = new Ram(AddressMap.WORK_RAM_SIZE);
+        ramController = new RamController(ram, AddressMap.WORK_RAM_START,
+                AddressMap.WORK_RAM_END);
+        echoRamController = new RamController(ram, AddressMap.ECHO_RAM_START,
+                AddressMap.ECHO_RAM_END);
+        bus = new Bus();
+        bus.attach(ramController);
+        bus.attach(echoRamController);
+
+    }
+
+    public Bus bus() {
+        return bus;
+
+    }
 
 }
