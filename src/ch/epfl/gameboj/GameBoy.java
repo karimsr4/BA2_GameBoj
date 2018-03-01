9cf8c7501329d3955a5a6343c235db4439df205a
Ahmedjjj <33035022+Ahmedjjj@users.noreply.github.com>
2018-03-01 16:00:46 +0100

    Game3asba

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -3,12 +3,24 @@ package ch.epfl.gameboj;
 import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
+/**
+ * @author Karim HADIDANE (271018)
+ * @author Ahmed JELLOULI (274056)
+ *
+ */
 public class GameBoy {
     private Ram ram;
     private RamController ramController;
     private RamController echoRamController;
     private Bus bus;
 
+    /**
+     * Construit une nouvelle Gameboy en créant ses composants : une ram et deux
+     * ram controllers et un bus et en attachant ces composants au bus
+     * 
+     * @param cartridge
+     *            jeu a jouer
+     */
     public GameBoy(Object cartridge) {
         ram = new Ram(AddressMap.WORK_RAM_SIZE);
         ramController = new RamController(ram, AddressMap.WORK_RAM_START,
@@ -21,6 +33,11 @@ public class GameBoy {
 
     }
 
+    /**
+     * retourne le bus de la gameboy
+     * 
+     * @return bus de la gameboy reliant ses composants
+     */
     public Bus bus() {
         return bus;
 
