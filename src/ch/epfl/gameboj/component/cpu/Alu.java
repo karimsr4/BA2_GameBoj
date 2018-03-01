ba3b5a340f66fe774fd6b0f1705aaad0b9ce081b
KARIM <karim.hadidane@epfl.ch>
2018-03-01 16:58:13 +0100

    Alu comments

--- src/ch/epfl/gameboj/component/cpu/Alu.java
+++ src/ch/epfl/gameboj/component/cpu/Alu.java
@@ -4,6 +4,10 @@ import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.bits.Bit;
 import ch.epfl.gameboj.bits.Bits;
 
+/**
+ * @author Karim HADIDANE (271018)
+ * @author Ahmed JELLOULI (274056)
+ */
 public final class Alu {
 
 	public enum RotDir {
@@ -14,27 +18,61 @@ public final class Alu {
 		UNUSED_0, UNUSED_1, UNUSED_2, UNUSED_3, C, H, N, Z
 	}
 
+	
+	
+	/**
+	 * 
+	 */
 	private Alu() {
 
 	}
 
+	
+	/**
+	 * retourne une valeur dont les bits correspondant aux différents fanions valent 1
+	 * @param z : fanion Z	
+	 * @param n : fanion N
+	 * @param h : fanion H
+	 * @param c : fanion C
+	 * @return une valeur dont les bits correspondant aux différents fanions valent 1
+	 */
 	public static int maskZNHC(boolean z, boolean n, boolean h, boolean c) {
 		return (z ? Flag.Z.mask() : 0) | (n ? Flag.N.mask() : 0) | (h ? Flag.H.mask() : 0) | (c ? Flag.C.mask() : 0);
 
 	}
 
+	/**
+	 * retourne la valeur contenue dans le paquet valeur/fanion donné
+	 * @param valueFlags : paquet valeur/fanion
+	 * @return la valeur associée au paquet
+	 * @throws IllegalArgumentException si valueFlags n'est pas appropriée
+	 */
 	public static int unpackValue(int valueFlags) {
 		Preconditions.checkArgument(Bits.clip(4, valueFlags) == 0 && Bits.extract(valueFlags, 24, 8) == 0);
 		return Bits.extract(valueFlags, 8, 16);
 
 	}
 
+	/**
+	 * retourne les fanions contenus dans le paquet valeur/fanion donné
+	 * @param valueFlags : paquet valeur/fanion
+	 * @return les fanions associés au paquet
+	 *  @throws IllegalArgumentException si valueFlags n'est pas appropriée
+	 */
 	public static int unpackFlags(int valueFlags) {
 		Preconditions.checkArgument(Bits.clip(4, valueFlags) == 0 && Bits.extract(valueFlags, 24, 8) == 0);
 		return (Bits.extract(valueFlags, 4, 4)) << 4;
 
 	}
 
+	/**
+	 * retourne la somme des deux valeurs 8 bits données et du bit de retenue initial c0 et les fanions Z0HC
+	 * @param l: entier de 8 bits representant le premier operande
+	 * @param r: entier de 8 bits representant le deuxieme operande
+	 * @param c0 : le bit de retenue initial
+	 * @return la somme des deux valeurs données et du bit de retenue initial  
+	 * @throws IllegalArgumentException si l'une des deux operandes n'est pas un entier de 8 bits
+	 */
 	public static int add(int l, int r, boolean c0) {
 
 		int result = Preconditions.checkBits8(l) + Preconditions.checkBits8(r) + Bits.set(0, 0, c0);
@@ -48,11 +86,27 @@ public final class Alu {
 
 	}
 
+	/**
+	 * retourne la somme des deux valeurs 8 bits données  et les fanions Z0HC
+	 * @param l: entier de 8 bits representant le premier operande
+	 * @param r: entier de 8 bits representant le deuxieme operande
+	 * @return la somme des deux valeurs données  
+	 * @throws IllegalArgumentException si l'une des deux operandes n'est pas un entier de 8 bits
+	 */
 	public static int add(int l, int r) {
 		return add(l, r, false);
 
 	}
 
+	/**
+	 * retourne la somme des deux valeurs 16 bits données et les fanions 00HC, 
+	 * avec H et C sont les fanions correspondant à l'addition des 8 bits de poids faible
+	 * @param l:  entier de 16 bits representant le premier operande
+	 * @param r:  entier de 16 bits representant le deuxieme operande
+	 * @return  la somme des deux valeurs et les fanions 00HC
+ 	 * @throws IllegalArgumentException si l'une des deux operandes n'est pas un entier de 16 bits
+ 
+	 */
 	public static int add16L(int l, int r) {
 
 		int result = Preconditions.checkBits16(l) + Preconditions.checkBits16(r);
@@ -64,6 +118,15 @@ public final class Alu {
 
 	}
 
+	/**
+	 * retourne la somme des deux valeurs 16 bits données et les fanions 00HC, 
+	 * avec H et C sont les fanions correspondant à l'addition des 8 bits de poids fort
+	 * @param l:  entier de 16 bits representant le premier operande
+	 * @param r:  entier de 16 bits representant le deuxieme operande
+	 * @return  la somme des deux valeurs et les fanions 00HC
+	 * @throws IllegalArgumentException si l'une des deux operandes n'est pas un entier de 16 bits
+
+	 */
 	public static int add16H(int l, int r) {
 
 		int result = Preconditions.checkBits16(l) + Preconditions.checkBits16(r);
@@ -77,6 +140,15 @@ public final class Alu {
 
 	}
 
+	/**
+	 * retourne la différence des valeurs de 8 bits données et du bit d'emprunt initial b0 et les fanions Z1HC,
+	* @param l:  entier de 8 bits representant le premier operande
+	 * @param r:  entier de 8 bits representant le deuxieme operande
+	 * @param b0: booleen representant le bit d'emprunt
+	 * @return
+ 	 * @throws IllegalArgumentException si l'une des deux operandes n'est pas un entier de 8 bits
+
+	 */
 	public static int sub(int l, int r, boolean b0) {
 
 		int result = Bits.clip(8, Preconditions.checkBits8(l) - (Preconditions.checkBits8(r) + Bits.set(0, 0, b0)));
@@ -88,10 +160,22 @@ public final class Alu {
 
 	}
 
+	/**
+	 * @param l
+	 * @param r
+	 * @return
+	 */
 	public static int sub(int l, int r) {
 		return sub(l, r, false);
 	}
 
+	/**
+	 * @param v
+	 * @param n
+	 * @param h
+	 * @param c
+	 * @return
+	 */
 	public static int bcdAdjust(int v, boolean n, boolean h, boolean c) {
 		boolean fixL = (h) || ((!(n)) && (Bits.clip(4, v) > 9));
 		boolean fixH = c || ((!n) && (v > 0x99));
@@ -101,6 +185,11 @@ public final class Alu {
 
 	}
 
+	/**
+	 * @param l
+	 * @param r
+	 * @return
+	 */
 	public static int and(int l, int r) {
 
 		int result = Preconditions.checkBits8(l) & Preconditions.checkBits8(r);
