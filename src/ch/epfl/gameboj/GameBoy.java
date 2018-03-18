2cdb0113653c864a1e44610e6005d70f903fe439
KARIM <karim.hadidane@epfl.ch>
2018-03-19 00:56:45 +0100

    etape 5

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -1,7 +1,6 @@
 package ch.epfl.gameboj;
 
-import java.util.Objects;
-
+import ch.epfl.gameboj.component.cpu.Cpu;
 import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
@@ -15,6 +14,7 @@ public class GameBoy {
     private RamController ramController;
     private RamController echoRamController;
     private Bus bus;
+    private Cpu cpu;
 
     /**
      * Construit une nouvelle Gameboy en créant ses composants : une ram et deux
@@ -28,11 +28,23 @@ public class GameBoy {
         ramController = new RamController(ram, AddressMap.WORK_RAM_START);
         echoRamController = new RamController(ram, AddressMap.ECHO_RAM_START,
                 AddressMap.ECHO_RAM_END);
+        cpu=new Cpu();
         bus = new Bus();
         bus.attach(ramController);
         bus.attach(echoRamController);
+        bus.attach(cpu);
 
     }
+    
+    
+    /**
+     * retourne le processeur du Game Boy
+     * @return le processeur 
+     */
+    public Cpu cpu() {
+        return this.cpu;
+    }
+    
 
     /**
      * retourne le bus de la gameboy
