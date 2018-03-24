05834448c8060180ffb4606191dec0fad9c5df3b
Ahmedjjj <33035022+Ahmedjjj@users.noreply.github.com>
2018-03-24 18:28:44 +0100

    STATA

--- src/ch/epfl/gameboj/component/Timer.java
+++ src/ch/epfl/gameboj/component/Timer.java
@@ -2,22 +2,22 @@ package ch.epfl.gameboj.component;
 
 import java.util.Objects;
 
+import ch.epfl.gameboj.bits.Bits;
 import ch.epfl.gameboj.component.cpu.Cpu;
 
 public final class Timer implements Clocked, Component {
 
-    
     private Cpu cpu;
-    
-    
+    private int FIMA;
+    private int TIMA;
+    private int TMA;
+    private int TAC;
+
     public Timer(Cpu cpu) {
-        Objects.requireNonNull(cpu)
+        Objects.requireNonNull(cpu);
+        this.cpu = cpu;
     }
-    
-    
-    
-    
-    
+
     @Override
     public int read(int address) {
         // TODO Auto-generated method stub
@@ -27,13 +27,39 @@ public final class Timer implements Clocked, Component {
     @Override
     public void write(int address, int data) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void cycle(long cycle) {
         // TODO Auto-generated method stub
-        
+
+    }
+
+    private boolean state() {
+        return Bits.test(TAC, 3) && Bits.test(FIMA, bitIndex());
+
+    }
+
+    private void incIfChange(boolean previousState) {
+        if ((previousState) && (!state()))
+            TIMA++;
+
+    }
+
+    private int bitIndex() {
+        switch (Bits.clip(2, TAC)) {
+        case 0b00:
+            return 9;
+        case 0b01:
+            return 3;
+        case 0b10:
+            return 5;
+        case 0b11:
+            return 7;
+        default:
+            return 0;
+        }
     }
 
 }
