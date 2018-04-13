cbccbe124e26f4f31ef1fea7ef989ca9c3645678
Ahmedjjj <ahmed.jellouli@epfl.ch>
2018-04-13 15:06:20 +0200

    dddd

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -1,10 +1,32 @@
 package ch.epfl.gameboj.bits;
 
+import static ch.epfl.gameboj.Preconditions.*;
+
+import java.util.Arrays;
+
+/**
+ * @author Ahmed
+ *
+ */
 public final class BitVector {
-    private final int vector;
+    private final int[] vector;
 
+    /**
+     * @param size
+     * @param initialValue
+     */
     public BitVector(int size, boolean initialValue) {
-
+        checkArgument((size > 0) && (size % 32 == 0));
+        vector = new int[size / 32];
+        int fillingValue = initialValue ? 0xFFFFFFFF : 0;
+        Arrays.fill(vector, fillingValue);
     }
+    /**
+     * @param size
+     */
+    public BitVector(int size) {
+        this(size,false);
+    }
+    
 
 }
