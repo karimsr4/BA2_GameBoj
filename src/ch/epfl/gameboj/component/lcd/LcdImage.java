90847fd35e8f40ea0656fdc12c7af5d6050a6772
KARIM <karim.hadidane@epfl.ch>
2018-04-26 16:05:11 +0200

    dddd

--- src/ch/epfl/gameboj/component/lcd/LcdImage.java
+++ src/ch/epfl/gameboj/component/lcd/LcdImage.java
@@ -3,9 +3,13 @@ package ch.epfl.gameboj.component.lcd;
 import static ch.epfl.gameboj.Preconditions.checkArgument;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+
+import ch.epfl.gameboj.Preconditions;
 
 /**
  * @author ADMIN
@@ -53,7 +57,42 @@ public final class LcdImage {
 
     public final static class Builder {
         
+        private final LcdImageLine[] lines;
+        private final int height, width;
+        private boolean isBuilded;
+        
+        
+        Builder(int width, int height){
+            lines =new LcdImageLine[height];
+            for(int i=0;i<height; i++) {
+                lines[i]=new LcdImageLine.Builder(width).build();
+            }
+            
+            this.width=width;
+            this.height=height;
+            isBuilded = false;
+        }
+        
+        
+        
+        public Builder setLine(int index , LcdImageLine newLine) {
+            if (isBuilded)
+                throw new IllegalStateException();
+            
+            Objects.checkIndex(index, height);
+            Preconditions.checkArgument(newLine.size()==width);
+            lines[index]=newLine;
+            return this;
+            
+        }
 
+        
+        public LcdImage build() {
+            if (isBuilded)
+                throw new IllegalStateException();
+            
+            return new LcdImage(width, height, Arrays.asList(lines));
+        }
     }
 
 }
