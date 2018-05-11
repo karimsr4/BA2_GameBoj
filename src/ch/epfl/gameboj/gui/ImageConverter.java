119dd1db69010f6ee35401e2bb89428e74808323
jellouli <jellouli@icin3pc42.epfl.ch>
2018-05-11 12:42:07 +0200

    3aasba

--- src/ch/epfl/gameboj/gui/ImageConverter.java
+++ src/ch/epfl/gameboj/gui/ImageConverter.java
@@ -1,5 +1,14 @@
 package ch.epfl.gameboj.gui;
+import ch.epfl.gameboj.component.lcd.LcdController;
+import ch.epfl.gameboj.component.lcd.LcdImage;
+import javafx.scene.image.Image ;
+import javafx.scene.image.WritableImage;
 
-public class ImageConverter {
+public final class ImageConverter {
+    public static  Image convert (LcdImage other) {
+        WritableImage result = new WritableImage(LcdController.LCD_WIDTH, LcdController.LCD_HEIGHT);
+        
+        
+    }
 
 }
